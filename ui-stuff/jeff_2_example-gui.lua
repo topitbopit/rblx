1c97ed4ebe455cbefd087c9c5856f0d4d8ab2123
topit <82128920+topitbopit@users.noreply.github.com>
2021-10-22 12:17:07 -0700

    Update jeff_2_example-gui.lua

--- ui-stuff/jeff_2_example-gui.lua
+++ ui-stuff/jeff_2_example-gui.lua
@@ -12,16 +12,30 @@ local window = ui:NewWindow("Example Speed GUI", 400, 300)
 -- Main menu
 local menu = window:NewMenu("Modules")
 
-local toggle = menu:NewToggle("Toggle speed")
+local toggle = menu:NewToggle("Toggle")
+local mode = menu:NewDropdown("Speed method", {"CFrame", "Walkspeed"})
+local amount = menu:NewSlider("Speed amount", 1, 200, 16)
 
 
+menu.OnEnable:Connect(function() 
+    rs:UnbindFromRenderStep("Jeff2_ExampleSpeed")
+    local _,idx = mode:GetSelection()
+    
+    if idx == 1 then --cframe
+            
+    elseif idx == 2 then --walkspeed
+         
+                
+    end
+end)
+
+menu.OnDisable:Connect(function() 
+    rs:UnbindFromRenderStep("Jeff2_ExampleSpeed")
+end)
 
 --Call ui:Ready() to finish everything off
 ui:Ready()
 
-
-
 ui.Exiting:Connect(function() 
-    rs:UnbindFromRenderStep("Jeff2_ExampleSpeed")
-      
+    rs:UnbindFromRenderStep("Jeff2_ExampleSpeed") 
 end)
